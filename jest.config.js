29f39a5c4f21ac056ae56a349e272e4e1ac53965
mando <nsa06031@gmail.com>
2020-12-31 18:29:46 +0900

    chore: jest에 typescript 적용

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx'],
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
--- webpack.common.js
+++ webpack.common.js
@@ -8,24 +8,7 @@ module.exports = {
       {
         test: /\.m?(j|t)sx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/typescript',
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    ie: '10',
-                  },
-                  useBuiltIns: 'usage',
-                  corejs: 3,
-                },
-              ],
-            ],
-          },
-        },
+        loader: 'babel-loader',
       },
     ],
   },
