85070f9208c3ab1c519559b0df5b997750214121
mando <nsa06031@gmail.com>
2020-12-31 16:59:14 +0900

    chore: 빌드 프로세스에 타입 체크(tsc)와 @babel/preset-typescript 추가

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,18 @@
 module.exports = {
   entry: './src/index.js',
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+  },
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.m?(j|t)sx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: [
+              '@babel/typescript',
               [
                 '@babel/preset-env',
                 {
