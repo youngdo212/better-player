9917c1aeb1e12b8f9163eb52b2ae7bf04527f908
mando <nsa06031@gmail.com>
2021-01-06 21:56:09 +0900

    feat: Video 클래스에 타입스크립트 적용

--- src/base/video/Video.test.ts
+++ src/base/video/Video.test.ts
@@ -1,74 +1,98 @@
+import config from '../../config/defaults';
 import Video from './Video';
 
 it('config이라는 이름의 환경 설정 속성을 가진다', () => {
-  const config = {};
-  const video = new Video(config);
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.config).toBe(config);
 });
 
 it('isPaused() 메소드가 기본 값을 가진다', () => {
-  const video = new Video({});
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.isPaused()).toBe(true);
 });
 
 it('play() 메소드를 가진다', () => {
-  const video = new Video({});
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.play).toBeDefined();
 });
 
 it('pause() 메소드를 가진다', () => {
-  const video = new Video({});
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.pause).toBeDefined();
 });
 
 it('getDuration() 메소드를 가진다', () => {
-  const video = new Video({});
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.getDuration).toBeDefined();
 });
 
 it('getCurrentTime() 메소드를 가진다', () => {
-  const video = new Video({});
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.getCurrentTime).toBeDefined();
 });
 
 it('seek() 메소드를 가진다', () => {
-  const video = new Video({});
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.seek).toBeDefined();
 });
 
 it('setVolume() 메소드를 가진다', () => {
-  const video = new Video({});
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.setVolume).toBeDefined();
 });
 
 it('getVolume() 메소드를 가진다', () => {
-  const video = new Video({});
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.getVolume).toBeDefined();
 });
 
 it('mute() 메소드를 가진다', () => {
-  const video = new Video({});
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.mute).toBeDefined();
 });
 
 it('unmute() 메소드를 가진다', () => {
-  const video = new Video({});
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.unmute).toBeDefined();
 });
 
 it('reload() 메소드를 가진다', () => {
-  const video = new Video({});
+  class TestVideo extends Video {}
+
+  const video = new TestVideo(config);
 
   expect(video.reload).toBeDefined();
 });
--- src/base/video/Video.ts
+++ src/base/video/Video.ts
@@ -1,104 +1,117 @@
 /** @module base/video */
 
 import UIObject from '../ui-object';
+import type { Config } from '../../types';
 
 /**
- * 비디오 플레이어가 컨트롤할 멤버를 정의한 인터페이스와 같은 클래스
+ * 비디오 플레이어가 컨트롤할 비디오 클래스
  * @extends UIObject
  */
-export default class Video extends UIObject {
+export default abstract class Video extends UIObject {
+  /**
+   * 비디오 플레이어 환경설정 객체
+   */
+  private _config: Config;
+
   /**
    * 비디오의 환경 설정 객체
-   * @return object
    */
-  get config() {
+  get config(): Config {
     return this._config;
   }
 
   /**
    * 재생 가능한 상태인지를 나타낸다.
    * 이 속성을 이용해 플러그인들이 자신의 렌더링 여부를 결정한다(e.g. Controller).
-   * @returns {boolean}
    */
-  get canPlay() {
+  get canPlay(): boolean {
     return false;
   }
 
   /**
    * 환경 설정을 저장하고 인스턴스를 생성한다.
-   * @param {object} config
    */
-  constructor(config) {
+  constructor(config: Config) {
     super();
     this._config = config;
   }
 
   /**
    * 재생 여부를 반환한다
-   * @returns {boolean}
    */
-  isPaused() {
+  isPaused(): boolean {
     return true;
   }
 
   /**
    * 영상의 총 길이를 반환한다.
-   * @returns {number}
    */
-  getDuration() {
+  getDuration(): number {
     return NaN;
   }
 
   /**
    * 영상의 현재 시간을 반환한다
-   * @returns {number}
    */
-  getCurrentTime() {
+  getCurrentTime(): number {
     return 0;
   }
 
   /**
    * 영상의 볼륨을 반환한다
-   * @returns {number}
    */
-  getVolume() {
+  getVolume(): number {
     return 1;
   }
 
   /**
    * 비디오를 재생한다
    */
-  play() {}
+  play(): void {
+    // do nothing.
+  }
 
   /**
    * 비디오를 일시 정지한다.
    */
-  pause() {}
+  pause(): void {
+    // do nothing.
+  }
 
   /**
    * 영상을 탐색한다
-   * @param {number} time
+   * @param time 초 단위의 소수점 숫자
    */
-  seek(time) {} // eslint-disable-line
+  seek(time: number): void {
+    // do nothing.
+  }
 
   /**
    * 볼륨을 조절한다
-   * @param {number} volume
+   * @param volume 0 이상 1 이하의 값
    */
-  setVolume(volume) {} // eslint-disable-line
+  setVolume(volume: number): void {
+    // do nothing.
+  }
 
   /**
    * 음소거한다.
    */
-  mute() {}
+  mute(): void {
+    // do nothing.
+  }
 
   /**
    * 음소거를 해제한다.
    */
-  unmute() {}
+  unmute(): void {
+    // do nothing.
+  }
 
   /**
    * 설정된 url로 비디오를 다시 로드한다.
    */
-  reload() {}
+  reload(): void {
+    // do nothing.
+  }
 }
